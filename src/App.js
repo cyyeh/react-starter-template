4baf9c5c0224cdc8cf6409ba69632745bedd07a0
ChihYu Yeh <cyyeh@ChihYus-MacBook-Pro.local>
2019-11-27 20:04:23 +0800

    setup react starter project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Root>
+    This is React App Template based on Create-React-App with following libraries added: styled-components and material-ui!
+    <br/>
+    Enjoy!
+  </Root>
+)
 
-export default App;
+const Root = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-size: 18px;
+  font-weight: 600;
+`
+
+export default App
